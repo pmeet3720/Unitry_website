7b048d9e3eed4560bf5d5cf433eb2ef467a683d1
«vraj <vrajcpatel1111@gmail.com>
2024-03-17 01:17:23 +0530

    Updated Button location

--- src/AboutUsSection.js
+++ src/AboutUsSection.js
@@ -9,9 +9,9 @@ const AboutUsSection = () => (
       <p>Founded in 2007, Our Company has been dedicated to providing high-quality uniforms for various sectors including schools, healthcare, hospitality, and more. With over 17 years of experience, we pride ourselves on our commitment to excellence and customer satisfaction.</p>
       <p>Our mission is to empower professionals across industries with uniforms that offer both style and functionality. We believe in the power of a well-crafted uniform to make every day extraordinary.</p>
       <p>Contact us to learn more about how we can serve your organization's needs.</p>
+      <button className="cta-button-ABUS">Learn More</button>
     </div>
     <br></br>
-    <button className="cta-button-ABUS">Learn More</button>
   </section>
 );
 
